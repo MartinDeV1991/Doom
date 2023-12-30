3186e44628b06a7cfe0837706aeb3924ee4ec706
Martin <martin_de_v@hotmail.com>
2023-12-30 17:57:59 +0100

    check wall collision

--- player.py
+++ player.py
@@ -36,8 +36,7 @@ class Player:
             dx += -speed_sin
             dy += speed_cos
 
-        self.x += dx
-        self.y += dy
+        self.check_wall_collision(dx, dy)
         
         if keys[pg.K_LEFT]:
             self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
@@ -45,6 +44,16 @@ class Player:
             self.angle += PLAYER_ROT_SPEED * self.game.delta_time
         self.angle %= math.tau
     
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+    
+    def check_wall_collision(self, dx, dy):
+        scale = 1
+        if self.check_wall(int(self.x + dx * scale), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy * scale)):
+            self.y += dy
+
     def draw(self):
         pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
                     (self.x * 100 + WIDTH * math.cos(self.angle),
